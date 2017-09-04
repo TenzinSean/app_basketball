40728592a2b0890fe2884db4483e591a412429be
tenzinsean <tenzin.jeunesse@gmail.com>
2017-09-04 13:15:17 +0200

    added new files

--- Basket_App/Basket_App/ViewController.swift
+++ Basket_App/Basket_App/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swossh: UIImageView!
+    
+    @IBOutlet weak var bg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+    
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
